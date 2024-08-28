95dcf8475ceded628c96038f59b5b6f389b615c3
paurakhsharma <paurakh011@gmail.com>
2024-08-28 15:39:00 +0545

    refactor: remove unnecessary export

--- lib/story_cms_client.dart
+++ lib/story_cms_client.dart
@@ -2,7 +2,6 @@ library story_cms_client;
 
 export './client.dart';
 export './features/pages/pages_loader.dart';
-export './features/pages/pages_manager.dart';
 export './models/page_models.dart';
 export './services/network_service.dart';
 export 'features/pages/selected_page_loader.dart';
